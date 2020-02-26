4fb8c68e718bc34b0193042622f8e6d7373f2a64
Oleksii <46541097+OleksiiFushtei@users.noreply.github.com>
2020-02-26 15:40:50 +0100

    Inquiries and sending email functionality done
    Now users can make an inquiry and realtor will receive an email about it

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.mail import send_mail
+from django.conf import settings
 
 from .models import Contact
 
@@ -14,8 +16,26 @@ def contact(request):
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
 
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(request, 'You have already made an inquiry for this listing')
+                return redirect('/listings/'+listing_id)
+
         contact = Contact(listing_id=listing_id, listing=listing, name=name, email=email, phone=phone, message=message, user_id=user_id)
         contact.save()
+
+        send_mail(
+            subject='Property Listing Inquiry',
+            message='There has been an inquiry for ' + listing + '. Sign into the admin panel for more info',
+            from_email='absolutely.valid.and.real.email@gmail.com',
+            recipient_list=[
+                realtor_email,
+                'olexiifushtey@gmail.com',
+            ],
+            fail_silently=False
+        )
 
         messages.success(request, 'Your request has been submitted, a realtor will contact you soon')
         return redirect('/listings/'+listing_id)
